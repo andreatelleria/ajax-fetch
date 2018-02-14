3c7be1ec2bc89f5b4c1715c7853f19150a5276fe
llandreall <andrea.telleria@gmail.com>
2018-02-14 09:44:01 -0500

    Trabajando en AJAX con fetch

---
+++ js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+  const form = document.getElementById('submit-btn');
+  const searchField = document.getElementById('search-keyword');
+  const responseContainer = document.getElementById('response-container');
+  let searchedForText;
+
+});
